0389ea6a3850d715414be3fe4c6c0654cd2c9504
Pranjal Gupta <pranjalg2004@gmail.com>
2026-05-21 11:39:23 +0530

    fix: fallback API URL to EC2 instead of localhost

--- excel_validation_frontend/src/config.js
+++ excel_validation_frontend/src/config.js
@@ -1,2 +1,2 @@
-const API_BASE = process.env.REACT_APP_API_URL || "http://127.0.0.1:8001";
+const API_BASE = process.env.REACT_APP_API_URL || "https://sahidata.duckdns.org";
 export default API_BASE;
